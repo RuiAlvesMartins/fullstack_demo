bb86a2cc52cd67ecb4bcfa57bb394d9c9b0ccf52
Rui Martins <102309086+RuiAlvesMartins@users.noreply.github.com>
2023-03-19 13:29:55 +0000

    connected to mongoDB

--- src/main/java/dev/rui/fullstack_demo/FullstackDemoApplication.java
+++ src/main/java/dev/rui/fullstack_demo/FullstackDemoApplication.java
@@ -2,12 +2,20 @@ package dev.rui.fullstack_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+// @RestController
 public class FullstackDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FullstackDemoApplication.class, args);
+	SpringApplication.run(FullstackDemoApplication.class, args);
 	}
+
+	// @GetMapping("/")
+	// public String apiRoot(){
+	// 	return "Hello world!";
+	// }
 
 }
